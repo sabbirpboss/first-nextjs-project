449b2dac1c4ccb144d59911f9b937b590a82395f
sabbirpboss <sabbir1pro@gmail.com>
2022-08-11 02:03:28 +0600

    nested route fun

---
+++ pages/Blogs/Blog1.js
@@ -0,0 +1,9 @@
+const Blog1 = () => {
+    return (
+        <div>
+            Blog1 is a nested route component that is rendered when the path is /blogs/blog1
+        </div>
+    );
+}
+
+export default Blog1;
